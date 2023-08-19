699d740ecc28c03f962e5338c7caf6a0dc0a8ec2
Swaraj <syadav481@outlook.com>
2023-08-18 23:20:04 -0700

    Problem: 456

--- lcscript.sh
+++ lcscript.sh
@@ -11,10 +11,9 @@ cpp_filename="${number}.cpp"
 py_filename="${number}.py"
 
 touch "$cpp_filename"
-touch "$py_filename"
+cp template_lc.cpp "$cpp_filename"
 
 echo "Created files: $cpp_filename and $py_filename"
 
 git add . 
 git commit -m "Problem: $number"
-git push 
