b8d0a31391138066ad026ed0ff1207a6e99792ea
Swaraj <syadav481@outlook.com>
2023-09-11 15:51:53 -0400

    Problem: 2616

--- 561.cpp
+++ 561.cpp
@@ -8,5 +8,14 @@
 #include <vector>
 
 using namespace std;
-
+class Solution {
+public:
+  int arrayPairSum(vector<int> &nums) {
+    sort(nums.begin(), nums.end());
+    int ans = 0;
+    for (int i = 0; i < nums.size(); i += 2)
+      ans += nums[i];
+    return ans;
+  }
+};
 int main() {}
