8742e2616f3f2e5a9bae0c7bc56117eb4a9ee6ad
Swaraj <syadav481@outlook.com>
2023-09-13 09:11:29 -0400

    Problem: 981

--- 81.cpp
+++ 81.cpp
@@ -8,5 +8,42 @@
 #include <vector>
 
 using namespace std;
-
+class Solution {
+public:
+  bool search(vector<int> &nums, int target) {
+    int l = 0, r = nums.size() - 1;
+    while (l < r) {
+      while (l < nums.size() - 1 && nums[l] == nums[l + 1]) {
+        l += 1;
+      }
+      while (r > 0 && nums[r] == nums[r - 1]) {
+        r -= 1;
+      }
+      int m = (r - l) / 2 + l;
+      if (nums[m] > nums[r]) {
+        l = m + 1;
+      } else {
+        r = m;
+      }
+    }
+    int pivot = l;
+    l = 0, r = nums.size() - 1;
+    if (nums[pivot] <= target && target <= nums[r]) {
+      l = pivot;
+    } else {
+      r = pivot;
+    }
+    while (l <= r) {
+      int m = (r - l) / 2 + l;
+      if (target < nums[m]) {
+        r = m - 1;
+      } else if (target > nums[m]) {
+        l = m + 1;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+};
 int main() {}
