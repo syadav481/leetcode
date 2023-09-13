0205bdea726bd9296720a9d2a063174a655693d3
Swaraj <syadav481@outlook.com>
2023-09-13 09:09:38 -0400

    Problem: 81

--- 1816.cpp
+++ 1816.cpp
@@ -8,5 +8,21 @@
 #include <vector>
 
 using namespace std;
-
+class Solution {
+public:
+  string truncateSentence(string s, int k) {
+    string ans;
+    for (char c : s) {
+      if (c == ' ') {
+        k -= 1;
+      }
+      if (k == 0) {
+        return ans;
+      } else {
+        ans.push_back(c);
+      }
+    }
+    return ans;
+  }
+};
 int main() {}
