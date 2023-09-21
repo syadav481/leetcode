7e0fb2ced7acec6c466db973823c0e2a6dfd7526
Swaraj <syadav481@outlook.com>
2023-09-21 01:15:02 -0400

    Problem: 174

--- 141.cpp
+++ 141.cpp
@@ -8,5 +8,27 @@
 #include <vector>
 
 using namespace std;
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+ 	ListNode* s = head; 
+	ListNode* f = head; 
+	while (f and f->next) {
+		s = s->next; 
+		f = f->next->next; 
+		if (s == f){
+			return true; 
+		}
+	}
+	return false; 
+    }
+};
 int main() {}
