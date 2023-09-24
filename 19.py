2fb5d4f061f338df4accdbc1ddd4db841447a946
Swaraj <syadav481@outlook.com>
2023-09-23 21:27:26 -0400

    Problem: 226

--- 19.py
+++ 19.py
@@ -5,29 +5,4 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        if not head.next:
-            return head if n == 0 else None
-        if not head.next.next:
-            if n == 0:
-                return head
-            elif n == 1:
-                head.next = None
-                return head
-            else:
-                return head
-        sz = 0
-        curr = head
-        while curr:
-            curr = curr.next
-            sz += 1
-        first, second = head, head.next
-        sz -= 1
-        while second.next and sz > 0:
-            first = first.next
-            second = second.next
-            sz -= 1
-            print(first.val)
-            print(second.val)
-            print("---")
-        first.next = second.next
-        return head
+        
