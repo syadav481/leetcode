52bd717c4131f07dd3ab32954b63d2710dc94760
Swaraj <syadav481@outlook.com>
2023-09-23 21:24:46 -0400

    Problem: 19

--- 19.cpp
+++ 19.cpp
@@ -8,37 +8,5 @@
 #include <vector>
 
 using namespace std;
-struct ListNode {
-  int val;
-  ListNode *next;
-  ListNode() : val(0), next(nullptr) {}
-  ListNode(int x) : val(x), next(nullptr) {}
-  ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
 
-class Solution {
-public:
-  ListNode *removeNthFromEnd(ListNode *head, int n) {
-    if (!head) {
-      return head;
-    }
-    if (!head->next && n > 0) {
-      return nullptr;
-    } else {
-      return head;
-    }
-    auto slow = head;
-    auto fast = head;
-    for (int i = 0; i < n && head; ++i) {
-      fast = fast->next;
-    }
-    fast = fast->next;
-    while (fast) {
-      fast = fast->next;
-      slow = slow->next;
-    }
-    slow->next = slow->next->next;
-    return head;
-  }
-};
 int main() {}
