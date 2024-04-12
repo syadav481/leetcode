a27339e44b914dbe49862e8472c4559a068527fe
Swaraj Yadav <syadav481@outlook.com>
2024-04-12 03:38:46 -0400

    Anotha one

--- 79.cpp
+++ 79.cpp
@@ -4,7 +4,42 @@ using std::string;
 using std::vector;
 
 class Solution {
+  string target;
+
 public:
-  bool exist(vector<vector<char>> &board, string word) {}
-  bool backtrack(vector<vector<char>> &board, int row, int col) {}
+  bool exist(vector<vector<char>> &board, string word) {
+    target = word;
+    for (size_t r = 0; r < board.size(); ++r) {
+      for (size_t c = 0; c < board[r].size(); ++c) {
+        if (backtrack(board, r, c, 0)) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+  // Index keeps track of where in the current word you are
+  bool backtrack(vector<vector<char>> &board, size_t row, size_t col,
+                 size_t index) {
+    if (index == target.size()) {
+      return true;
+    }
+    if (row > board.size() || col > board[row].size()) {
+      return false;
+    }
+    char original_value = board[row][col];
+    if (original_value != target[index]) {
+      return false;
+    }
+    // mark current position as visited
+    board[row][col] = '#';
+    // recurse
+    bool ans = backtrack(board, row + 1, col, index + 1) ||
+               backtrack(board, row - 1, col, index + 1) ||
+               backtrack(board, row, col + 1, index + 1) ||
+               backtrack(board, row, col - 1, index + 1);
+    // unmark current position
+    board[row][col] = original_value;
+    return ans;
+  }
 };
